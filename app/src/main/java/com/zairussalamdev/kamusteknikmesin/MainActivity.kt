b831cf95631e57be67461eb09b72f7392924a387
ibrahimsyah <ibra.himsyah10@gmail.com>
2019-12-07 21:56:22 +0700

    Add Search Activity

--- app/src/main/java/com/zairussalamdev/kamusteknikmesin/MainActivity.kt
+++ app/src/main/java/com/zairussalamdev/kamusteknikmesin/MainActivity.kt
@@ -11,6 +11,7 @@ import com.google.firebase.database.ValueEventListener
 import com.zairussalamdev.kamusteknikmesin.model.Kategori
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
@@ -31,19 +32,19 @@ class MainActivity : AppCompatActivity() {
         menu2.onClick {
             toast("Menu2 Di Klik")
         }
-        search.onClick{
-            toast("Search Di Klik")
+        search.onClick {
+            startActivity<SearchActivity>();
         }
         val ref = FirebaseDatabase.getInstance().getReference("kategori")
-        ref.addValueEventListener(object : ValueEventListener{
+        ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
-                for(h in p0.children){
+                for (h in p0.children) {
 //                    TODO("Add Action after Querying DB")
                 }
             }
+
             override fun onCancelled(p0: DatabaseError) {
             }
-
 
 
         })
