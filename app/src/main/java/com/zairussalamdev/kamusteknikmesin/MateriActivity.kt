8aca9dc2f6118d775b95f48bcca1c9dc8f54aaf4
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2019-12-15 08:03:54 +0700

    Add Materi View

--- app/src/main/java/com/zairussalamdev/kamusteknikmesin/MateriActivity.kt
+++ app/src/main/java/com/zairussalamdev/kamusteknikmesin/MateriActivity.kt
@@ -1,15 +1,16 @@
 package com.zairussalamdev.kamusteknikmesin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.zairussalamdev.kamusteknikmesin.model.Kategori
+import com.zairussalamdev.kamusteknikmesin.adapter.MateriAdapter
 import com.zairussalamdev.kamusteknikmesin.model.Materi
-import org.jetbrains.anko.toast
+import kotlinx.android.synthetic.main.activity_materi.*
 
 class MateriActivity : AppCompatActivity() {
 
@@ -19,18 +20,23 @@ class MateriActivity : AppCompatActivity() {
 
         val idKategori = intent.getIntExtra("id_kategori", 0).toString()
         Log.d("Materi", "Kategori: $idKategori")
-        val items : MutableList<Materi> = mutableListOf()
+        val items: MutableList<Materi> = mutableListOf()
+
         val database = FirebaseDatabase.getInstance()
-        val ref = database.getReference("/materi").orderByChild("id_kategori").startAt(idKategori).endAt(idKategori)
+        val ref = database.getReference("/materi").orderByChild("id_kategori").startAt(idKategori)
+            .endAt(idKategori)
         ref.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
 
             override fun onDataChange(p0: DataSnapshot) {
-                for(h  in p0.children){
+                for (h in p0.children) {
                     val item = h.getValue(Materi::class.java)
-                    item?.let { Log.d("Materi", it.title)}
+                    item?.let { items.add(item)}
                 }
             }
         })
+        rvMateri.layoutManager = LinearLayoutManager(this)
+        rvMateri.setHasFixedSize(true)
+        rvMateri.adapter = MateriAdapter(this, items){}
     }
 }
--- app/src/main/java/com/zairussalamdev/kamusteknikmesin/adapter/MateriAdapter.kt
+++ app/src/main/java/com/zairussalamdev/kamusteknikmesin/adapter/MateriAdapter.kt
@@ -1,2 +1,39 @@
 package com.zairussalamdev.kamusteknikmesin.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
+import com.zairussalamdev.kamusteknikmesin.R
+import com.zairussalamdev.kamusteknikmesin.model.Materi
+import kotlinx.android.synthetic.main.item_rv.view.*
+
+class MateriAdapter (
+    val context: Context,
+    val items : List<Materi>,
+    val listener : (Materi)->Unit
+    ) : RecyclerView.Adapter<MateriAdapter.MateriViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MateriViewHolder
+    = MateriViewHolder(LayoutInflater.from(context).inflate(R.layout.item_rv, parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: MateriViewHolder, position: Int) {
+        holder.bindItem(items.get(position), listener)
+    }
+    class MateriViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        fun bindItem(item: Materi, listener: (Materi) -> Unit){
+            itemView.tvContent.text = item.title
+            val letter = item.title.substring(0,1)
+            val color = ColorGenerator.MATERIAL.randomColor
+            val drawable = TextDrawable.builder()
+                .buildRoundRect(letter, color,100)
+            itemView.Letter.setImageDrawable(drawable)
+            itemView.setOnClickListener{listener(item)}
+        }
+    }
+}
