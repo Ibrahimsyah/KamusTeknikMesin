a6022225582fc67f5e1cf3b7b84087728ab94a92
Ibrahimsyah Z <ibra.himsyah10@gmail.com>
2019-12-16 07:47:48 +0700

    Change App Architecture to MVP

--- app/src/main/java/com/zairussalamdev/kamusteknikmesin/SplashScreen.kt
+++ app/src/main/java/com/zairussalamdev/kamusteknikmesin/SplashScreen.kt
@@ -9,65 +9,27 @@ import com.google.firebase.database.ValueEventListener
 import com.zairussalamdev.kamusteknikmesin.db.db
 import com.zairussalamdev.kamusteknikmesin.model.Kategori
 import com.zairussalamdev.kamusteknikmesin.model.Materi
+import com.zairussalamdev.kamusteknikmesin.presenter.SplashPresenter
+import com.zairussalamdev.kamusteknikmesin.presenter.SplashView
 import org.jetbrains.anko.db.insert
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.startActivity
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity(), SplashView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
-        doAsync {
-            queryToDb()
-        }
+        val presenter = SplashPresenter(this, applicationContext)
+        presenter.queryData()
+    }
 
+    override fun showLoader() {}
+
+    override fun hideLoader() {
         startActivity<MainActivity>()
+        finish()
     }
 
-    private fun queryToDb() {
-        val database = FirebaseDatabase.getInstance()
-        var ref = database.getReference("/kategori")
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {}
-
-            override fun onDataChange(p0: DataSnapshot) {
-                for (h in p0.children) {
-                    val item = h.getValue(Kategori::class.java)
-                    item?.let {
-                        db.use {
-                            insert(
-                                Kategori.TABLE_KATEGORI,
-                                Kategori.ID_KATEGORI to item.id_kategori,
-                                Kategori.NAME to item.name
-                            )
-                        }
-                    }
-                }
-            }
-        })
-        ref = database.getReference("/materi")
-        ref.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                for (h in p0.children) {
-                    val item = h.getValue(Materi::class.java)
-                    item?.let {
-                        db.use {
-                            insert(
-                                Materi.TABLE_MATERI,
-                                Materi.ID_MATERI to item.id_materi,
-                                Materi.ID_KATEGORI to item.id_kategori,
-                                Materi.TITLE to item.title,
-                                Materi.CONTENT to item.content
-                            )
-                        }
-                    }
-                }
-            }
-        })
-    }
 }
---
+++ app/src/main/java/com/zairussalamdev/kamusteknikmesin/presenter/SplashPresenter.kt
@@ -0,0 +1,72 @@
+package com.zairussalamdev.kamusteknikmesin.presenter
+
+import android.content.Context
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.zairussalamdev.kamusteknikmesin.db.db
+import com.zairussalamdev.kamusteknikmesin.model.Kategori
+import com.zairussalamdev.kamusteknikmesin.model.Materi
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class SplashPresenter(
+    private val splashView: SplashView,
+    private val context: Context
+) {
+    fun queryData() {
+        splashView.showLoader()
+        doAsync {
+            val database = FirebaseDatabase.getInstance()
+            var ref = database.getReference("/kategori")
+            ref.addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {}
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    for (h in p0.children) {
+                        val item = h.getValue(Kategori::class.java)
+                        item?.let {
+                            context.db.use {
+                                insert(
+                                    Kategori.TABLE_KATEGORI,
+                                    Kategori.ID_KATEGORI to item.id_kategori,
+                                    Kategori.NAME to item.name
+                                )
+                            }
+                        }
+                    }
+                }
+            })
+            ref = database.getReference("/materi")
+            ref.addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    for (h in p0.children) {
+                        val item = h.getValue(Materi::class.java)
+                        item?.let {
+                            context.db.use {
+                                insert(
+                                    Materi.TABLE_MATERI,
+                                    Materi.ID_MATERI to item.id_materi,
+                                    Materi.ID_KATEGORI to item.id_kategori,
+                                    Materi.TITLE to item.title,
+                                    Materi.CONTENT to item.content
+                                )
+                            }
+                        }
+                    }
+                }
+            })
+            uiThread { splashView.hideLoader() }
+        }
+    }
+}
+
+interface SplashView {
+    fun showLoader()
+    fun hideLoader()
+}
